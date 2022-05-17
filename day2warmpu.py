89fd19c6b6a1bf4d5d9bd186a297a52fad046f57
dao <hmongkey12@users.noreply.github.com>
2022-05-17 19:59:08 +0000

    join and split

---
+++ day2warmpu.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+def main():
+    pi = 3.14
+    c = "circumference"
+    r = "radius"
+
+    print(f"To calculate the {c} of a circle, the formula is 2 x {pi} x {r}")
+
+main()
---
+++ dayweek.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+def doStuff():
+    userInput = input('what is your name?')
+    week = input('what is the day of the week?')
+    print("Hello, ", userInput, " Happy ", week)
+
+doStuff()
