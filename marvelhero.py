a58e1d08f5928d7f2c8ae26cb89c5bb9800b8cc7
dao <hmongkey12@users.noreply.github.com>
2022-05-18 19:48:45 +0000

    learned dictionaries

---
+++ marvelhero.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+Invincible = {
+    "Real Name" : "Mark Grayson",
+    "Creator" : ["Robert Kirkman", "Cory Walker"],
+    "Team": ["Teen Team", "The Pact", "Guardians of the Globe"],
+    "Superpowers":["Superhuman strength", "agility", "resilience" ,"stamina", "flight", "extended lifespan", "healing factor enhanced"],
+    "Father": "Omni Man" 
+    }
+
+Batman={'allies':['Robin','Catwoman','Alfred Pennyworth'],'location':'Gotham City','vehicles':'Batmobile','enemies':['Joker','Two-Face','Riddler'],'alias':'Bruce Wayne'}
+#print(Invincible)
+for key in Batman:
+    print(f"Key = {key} : Value: {Batman[key]}")
+
